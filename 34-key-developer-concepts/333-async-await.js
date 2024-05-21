372b6e160b61a2761bd0f3d375632d576f15e75b
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2024-05-21 07:51:24 +0700

    feat: update async await

---
+++ 34-key-developer-concepts/333-async-await.js
@@ -0,0 +1,26 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((users) => {
+    const firstUser = users[0]
+    console.log(firstUser)
+    return fetch('https://jsonplaceholder.typicode.com/posts?userId=' + firstUser.id)
+  })
+  .then((response) => response.json())
+  .then((posts) => console.log(posts))
+  .catch((error) => console.log(error))
+
+const myAsyncFunction = async () => {
+  try {
+    const userResponse = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await userResponse.json()
+    const secondUser = users[1]
+    console.log(secondUser)
+    const postsResponse = await fetch(
+      'https://jsonplaceholder.typicode.com/posts?userId=' + secondUser.id
+    )
+    const posts = await postsResponse.json()
+    console.log(posts)
+  } catch (err) {
+    console.log(err)
+  }
+}
